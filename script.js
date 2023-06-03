fc14f2043953222296814460f5c41fc7b969df57
Tarun-kumar-singh <tarunks2512@gmail.com>
2023-06-03 12:49:21 +0530

    modify files

--- script.js
+++
@@ -1,9 +0,0 @@
-
-
-const downloadResume = () => {
-    var link = document.createElement('a');
-    link.href = './resume.pdf';
-    link.download = 'resume.pdf';
-    link.dispatchEvent(new MouseEvent('click'));
-
-}
